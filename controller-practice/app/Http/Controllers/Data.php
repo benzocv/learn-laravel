8d70922df12a792fcfebadfb061cd1678a051199
benzocv <anzarahmedkhan99@gmail.com>
2021-05-16 13:12:17 +0530

    api creation with json output done

--- controller-practice/app/Http/Controllers/Data.php
+++ controller-practice/app/Http/Controllers/Data.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class Data extends Controller
 {
     public function index($name){
-        echo $name;
-        echo "this is index function from data controller";
+        // echo $name;
+        // echo "this is index function from data controller";
+        return ['name'=>"$name",'age'=>27];  //passing json output
     }
 }
