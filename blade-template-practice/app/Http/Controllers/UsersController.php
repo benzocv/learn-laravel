a7b3b539272667f55d5d4dd12ca3fa62181893f2
benzocv <anzarahmedkhan99@gmail.com>
2021-05-18 00:14:16 +0530

    if condition check done

--- blade-template-practice/app/Http/Controllers/UsersController.php
+++ blade-template-practice/app/Http/Controllers/UsersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     function viewLoad(){
-        return view('users',['users'=>['anil','ambani','modi','shah']]);
+        // return view('users',['users'=>['anil','ambani','modi','shah']]);
+        return view('users',['user'=>'anzar']);
     }
 }
--- blade-template-practice/resources/views/users.blade.php
+++ blade-template-practice/resources/views/users.blade.php
@@ -1,1 +1,5 @@
-<h1>Users page {{count($users)}}</h1>
+ {{-- <h1>Users page {{count($users)}}</h1> --}}
+
+@if($user == 'anzar')
+<h3>Hi {{$user}}</h3>
+@endif
