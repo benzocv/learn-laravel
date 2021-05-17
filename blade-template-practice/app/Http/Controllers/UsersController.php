0a8f544a7ff1f590b2b786e3a0f12570e65eebc4
benzocv <anzarahmedkhan99@gmail.com>
2021-05-18 00:30:22 +0530

    for each loop added

--- blade-template-practice/app/Http/Controllers/UsersController.php
+++ blade-template-practice/app/Http/Controllers/UsersController.php
@@ -8,6 +8,9 @@ class UsersController extends Controller
 {
     function viewLoad(){
         // return view('users',['users'=>['anil','ambani','modi','shah']]);
-        return view('users',['user'=>'sam']);
+        // return view('users',['user'=>'sam']);
+
+        $data = ['anzar','ahmed','khan','z','balooch'];
+        return view('users',['users'=>$data]);
     }
 }
