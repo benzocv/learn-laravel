64f5d0a7f617b80820c314e76b33688b99b9565c
benzocv <anzarahmedkhan99@gmail.com>
2021-05-18 00:23:56 +0530

    how to use for loop in blade engine added

--- blade-template-practice/resources/views/users.blade.php
+++ blade-template-practice/resources/views/users.blade.php
@@ -1,9 +1,15 @@
  {{-- <h1>Users page {{count($users)}}</h1> --}}
 
- @if($user == 'anzar')
+
+@if($user == 'anzar')
 <h3>Hi {{$user}}</h3>
 @elseif($user == 'sam')
 <h3>Hlo {{$user}}</h3>
 @else
 <h3>Unknown user</h3>
 @endif
+
+
+@for($i=0;$i<10;$i++)
+<h4>{{$i}}</h4>
+@endfor
