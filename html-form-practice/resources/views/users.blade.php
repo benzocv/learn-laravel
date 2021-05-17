dc607eda867b9251e7cf3e7ea1550f5a3dc46b62
benzocv <anzarahmedkhan99@gmail.com>
2021-05-18 01:23:05 +0530

    printing erros separtely done

--- html-form-practice/resources/views/users.blade.php
+++ html-form-practice/resources/views/users.blade.php
@@ -1,19 +1,24 @@
 <h1>User login</h1>
 
-
+{{--
 @if($errors->any())
 @foreach($errors->all() as $err)
 <li>{{$err}}</li>
 @endforeach
 @endif
+--}}
+
+
 
 
 <form action="users" method="POST">
 
 @csrf
 <input type="text" name="username" placeholder="Enter user Name">
+<span style="color:red">@error('username'){{$message}} @enderror</span>
 <br><br>
 <input type="password" name="userpassword" placeholder="Enter user password">
+<span style="color:red">@error('password'){{$message}} @enderror</span>
 <br><br>
 <button type="submit"> Login </button>
 </form>
