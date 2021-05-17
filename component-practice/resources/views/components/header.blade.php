53ad196a7604490609bbbbed811036158021020f
benzocv <anzarahmedkhan99@gmail.com>
2021-05-17 12:34:43 +0530

    two new views created and added

--- component-practice/resources/views/components/header.blade.php
+++ component-practice/resources/views/components/header.blade.php
@@ -1,3 +1,5 @@
 <div>
+
+<h1>This is from header</h1>
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid - Cato the Younger -->
 </div>
--- component-practice/routes/web.php
+++ component-practice/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/users', function () {
+    return view('users');
+});
