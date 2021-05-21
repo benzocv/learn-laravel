99166f6a4de647331f0f8c284b2c5575c5510769
benzocv <anzarahmedkhan99@gmail.com>
2021-05-21 15:58:18 +0530

    foreach loop done with the data

--- 13 Http-practice/resources/views/users.blade.php
+++ 13 Http-practice/resources/views/users.blade.php
@@ -5,5 +5,16 @@
     <td>Name</td>
     <td>Email</td>
     <td>Profile Photo</td>
+
 </tr>
+@foreach ($collection as $item)
+
+<tr>
+    <td>{{$item['id']}}</td>
+    <td>{{$item['first_name']}}</td>
+    <td>{{$item['email']}}</td>
+    <td><img src="{{$item['avatar']}}" alt=""></td>
+
+</tr>
+@endforeach
 </table> 
