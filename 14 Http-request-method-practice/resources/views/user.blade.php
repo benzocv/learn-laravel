7e00d1c2dfa46aaf01f710a615f1c76f9ecae626
benzocv <anzarahmedkhan99@gmail.com>
2021-05-21 17:18:02 +0530

    post method also added

--- 14 Http-request-method-practice/resources/views/user.blade.php
+++ 14 Http-request-method-practice/resources/views/user.blade.php
@@ -6,3 +6,14 @@
     <input type="text" name="password" placeholder="Enter Your Password" id=""><br><br>
     <button type="submit">Login</button>
 </form>
+
+
+<h1>POST Method User Login</h1>
+
+
+<form action="users" method="post">
+    @csrf
+    <input type="text" name="email" placeholder="Enter Your Email" id=""><br><br>
+    <input type="text" name="password" placeholder="Enter Your Password" id=""><br><br>
+    <button type="submit">Login</button>
+</form>
--- 14 Http-request-method-practice/routes/web.php
+++ 14 Http-request-method-practice/routes/web.php
@@ -25,3 +25,4 @@ Route::get('/user', function () {
 
 
 route::get("users",[UserController::class,'testRequest']);
+route::post("users",[UserController::class,'testRequest']);
