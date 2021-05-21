acd3e6ae3017c3085353c9e889bd425464b4fff9
benzocv <anzarahmedkhan99@gmail.com>
2021-05-21 17:00:32 +0530

    br added

--- 14 Http-request-method-practice/resources/views/user.blade.php
+++ 14 Http-request-method-practice/resources/views/user.blade.php
@@ -3,7 +3,7 @@
 
 <form action="">
 
-    <input type="text" name="user" id="">
-    <input type="text" name="user" id="">
+    <input type="text" name="user" id=""><br>
+    <input type="text" name="user" id=""><br>
     <button type="submit" name="user">Login</button>
 </form>
