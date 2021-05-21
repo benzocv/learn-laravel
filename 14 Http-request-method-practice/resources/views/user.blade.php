e7e1747c2418092b51943b745738ac3a80fca9c4
benzocv <anzarahmedkhan99@gmail.com>
2021-05-21 17:00:10 +0530

    form created scratch

--- 14 Http-request-method-practice/resources/views/user.blade.php
+++ 14 Http-request-method-practice/resources/views/user.blade.php
@@ -1,1 +1,9 @@
 <h1>User Login</h1>
+
+
+<form action="">
+
+    <input type="text" name="user" id="">
+    <input type="text" name="user" id="">
+    <button type="submit" name="user">Login</button>
+</form>
