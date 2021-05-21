ec2b5189bf4d242040fb262e4d605a109cec64b1
benzocv <anzarahmedkhan99@gmail.com>
2021-05-21 17:40:50 +0530

    controller added to handdle request

---
+++ 15 session-practice/app/Http/Controllers/UserAuth.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAuth extends Controller
+{
+    function UserLogin(Request $req){
+        $req->input();
+    }
+}
