8af4abc292784410eddaf6e3e301248a62d5e43c
benzocv <anzarahmedkhan99@gmail.com>
2021-05-21 17:57:12 +0530

    session stored and data viewd

--- 15 session-practice/app/Http/Controllers/UserAuth.php
+++ 15 session-practice/app/Http/Controllers/UserAuth.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class UserAuth extends Controller
 {
     function UserLogin(Request $req){
-        return $req->input();
+        // return $req->input();
+        
+        
+        $data = $req->input();
+        $req->session()->put('email',$data['email']);
+        echo session('email');
+        
         
     }
 }
