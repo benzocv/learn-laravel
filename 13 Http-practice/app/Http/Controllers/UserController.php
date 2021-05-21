52f21b435376c8c95fd8c7121f43d8fc979ed7ae
benzocv <anzarahmedkhan99@gmail.com>
2021-05-21 15:45:00 +0530

    now we are getting data from the api

--- 13 Http-practice/app/Http/Controllers/UserController.php
+++ 13 Http-practice/app/Http/Controllers/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
 
 class UserController extends Controller
 {
     function index(){
-        return "API data will be here";
+        return Http::get("https://reqres.in/api/users?page=1");
     }
 }
