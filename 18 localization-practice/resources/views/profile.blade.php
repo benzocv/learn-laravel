49d4967013028f188406673f7703b3fb7915276f
benzocv <anzarahmedkhan99@gmail.com>
2021-05-28 11:36:28 +0530

    working with dictionry

--- 18 localization-practice/resources/views/profile.blade.php
+++ 18 localization-practice/resources/views/profile.blade.php
@@ -1,5 +1,5 @@
-<h1>Hello this is profile page</h1>
+<h1>{{__('dictionary.welcome')}}</h1>
 
-<a href="">About</a>
-<a href="">contact</a>
-<a href="">List</a>
+<a href="">{{__('dictionary.about')}}</a>
+<a href="">{{__('dictionary.contact')}}</a>
+<a href="">{{__('dictionary.list')}}</a>
