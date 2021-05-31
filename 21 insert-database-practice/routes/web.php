44e6690695cc10163b905a890b5e4961805163a7
benzocv <anzarahmedkhan99@gmail.com>
2021-05-31 19:49:12 +0530

    route added

--- 21 insert-database-practice/routes/web.php
+++ 21 insert-database-practice/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/add-member', function () {
     return view('add-member');
 });
+
+Route::post('add-member',[MemberController::class,'addData']);
