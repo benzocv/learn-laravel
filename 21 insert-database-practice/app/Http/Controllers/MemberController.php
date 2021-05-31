63e557b8aa975ecc53ce9c439cc8eb081725c2a5
benzocv <anzarahmedkhan99@gmail.com>
2021-05-31 19:57:53 +0530

    data added into database done

--- 21 insert-database-practice/app/Http/Controllers/MemberController.php
+++ 21 insert-database-practice/app/Http/Controllers/MemberController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class MemberController extends Controller
 {
 function addData(Request $req){
+$member = new Member;
+$member->name = $req->name;
+$member->email = $req->email;
+$member->address = $req->address;
+$member->save();
 
 }
 }
