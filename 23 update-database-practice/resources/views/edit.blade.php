993f33d43a31b41470664f2c5ccf0fbe0e977845
benzocv <anzarahmedkhan99@gmail.com>
2021-06-05 23:48:47 +0530

    edit view created

---
+++ 23 update-database-practice/resources/views/edit.blade.php
@@ -0,0 +1,11 @@
+<h1>Member List</h1>
+
+<form action="" method="POST">
+
+@csrf
+<input type="text" name="name" placeholder="Enter You Name"><br><br>
+<input type="email" name="email" placeholder="Enter You Email"><br><br>
+<input type="text" name="address" placeholder="Enter You Address"><br><br>
+
+<button type="submit">Update</button>
+</form>
