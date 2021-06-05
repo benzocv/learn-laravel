83ee02620aadcc6fe75f5885e88270601fb59acd
benzocv <anzarahmedkhan99@gmail.com>
2021-06-05 23:53:03 +0530

    edit data form created

--- 23 update-database-practice/resources/views/edit.blade.php
+++ 23 update-database-practice/resources/views/edit.blade.php
@@ -3,9 +3,9 @@
 <form action="" method="POST">
 
 @csrf
-<input type="text" name="name" placeholder="Enter You Name"><br><br>
-<input type="email" name="email" placeholder="Enter You Email"><br><br>
-<input type="text" name="address" placeholder="Enter You Address"><br><br>
+<input type="text" name="name" placeholder="Enter You Name" value="{{$data['name']}}"><br><br>
+<input type="email" name="email" placeholder="Enter You Email" value="{{$data['email']}}"><br><br>
+<input type="text" name="address" placeholder="Enter You Address" value="{{$data['address']}}"><br><br>
 
 <button type="submit">Update</button>
 </form>
