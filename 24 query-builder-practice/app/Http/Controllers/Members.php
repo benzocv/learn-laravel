504fac07504dbc045ffdbe03b34155648ffb9f28
benzocv <anzarahmedkhan99@gmail.com>
2021-06-06 00:31:01 +0530

    all done data fetch

--- 24 query-builder-practice/app/Http/Controllers/Members.php
+++ 24 query-builder-practice/app/Http/Controllers/Members.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Members extends Controller
 {
     //
     function dbOperation(){
-        echo 'hlo';
+        return DB::table('members')->get();
     }
 }
--- 24 query-builder/note.php
+++ 24 query-builder/note.php
@@ -4,3 +4,9 @@
 // php artisan make:controller Members
 - create function in controller 
 - make route to the controller 
+- import db class
+//use Illuminate\Support\Facades\DB;
+- to fetch all data from the table use this in function 
+//function dbOperation(){
+//    return DB::table('members')->get();
+//}
