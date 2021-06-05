153d23d6c75838b8f42b0adb0b4c7341be56bb5f
benzocv <anzarahmedkhan99@gmail.com>
2021-06-05 23:33:36 +0530

    database update new project created

---
+++ 23 update-database-practice/app/Http/Controllers/MemberController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Member;
+
+class MemberController extends Controller
+{
+    function list(){
+        $data = Member::all();
+        return view('list',['members'=>$data]);
+    }
+
+    function delete($id){
+        $data = Member::find($id);
+        $data->delete();
+        return redirect('list');
+    }
+}
---
+++ 23 update-database-practice/resources/views/list.blade.php
@@ -0,0 +1,22 @@
+<h1>Member List</h1>
+
+<table>
+    <tr>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Address</td>
+        <td>Action</td>
+    </tr>
+
+    @foreach ($members as $item)
+    <tr>
+        <td>{{$item['id']}}</td>
+        <td>{{$item['name']}}</td>
+        <td>{{$item['email']}}</td>
+        <td>{{$item['address']}}</td>
+        <td><a href="delete/{{$item['id']}}">Delete</a></td>
+        
+    </tr>
+    @endforeach
+</table>
