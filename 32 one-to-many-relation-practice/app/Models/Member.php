ff1a8c1556d91e43cd3f68c6c256dbc6af5ddead
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 13:14:36 +0530

    data fetching done

--- 32 one-to-many-relation-practice/app/Models/Member.php
+++ 32 one-to-many-relation-practice/app/Models/Member.php
@@ -9,7 +9,7 @@ class Member extends Model
 {
     use HasFactory;
 
-    public function getCompany(){
-        return $this->hasOne('App\Models\Company');
+    public function getDevice(){
+        return $this->hasMany('App\Models\Device');
     }
 }
