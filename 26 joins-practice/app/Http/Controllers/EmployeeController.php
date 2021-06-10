acf68011eafe0d2967cf25b14dc7779b0e6a3b85
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 10:31:44 +0530

    database derived done

--- 26 joins-practice/app/Http/Controllers/EmployeeController.php
+++ 26 joins-practice/app/Http/Controllers/EmployeeController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EmployeeController extends Controller
 {
     function show(){
-        echo "this is from controller";
+        return DB::table('employee')->get();
     }
 }
