48cbb9a281815a3d772d0ff3c45a960811230e7e
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 10:28:50 +0530

    controller working fine

---
+++ 26 joins-practice/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    function show(){
+        echo "this is from controller";
+    }
+}
