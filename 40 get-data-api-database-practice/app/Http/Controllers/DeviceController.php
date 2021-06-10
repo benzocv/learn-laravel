f0ab008664ac5a23e6dbe630a75a36d0d678de75
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 23:58:25 +0530

    data derived from database with api done

--- 40 get-data-api-database-practice/app/Http/Controllers/DeviceController.php
+++ 40 get-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Device;
 
 class DeviceController extends Controller
 {
-    //
+    function list(){
+            return Device::all();
+    }
 }
