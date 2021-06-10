3159b1900173c943db0fcbd11e06116a706673f9
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 19:31:37 +0530

    derive data from other database done

--- 38 multiple-database-practice/app/Http/Controllers/DeviceController.php
+++ 38 multiple-database-practice/app/Http/Controllers/DeviceController.php
@@ -8,7 +8,10 @@ use Illuminate\Support\Facades\DB;
 class DeviceController extends Controller
 {
  function list(){
-     return DB::table('devices')->get();
+    //  return DB::table('devices')->get();
+
+     // to connect to extra connection database 
+     return DB::connection('mysql2')->table('employee')->get();
     
  }
 }
