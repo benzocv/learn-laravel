7ed21ade2e28eab856616e1b24ce2f18c2fc4688
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 12:22:09 +0530

    new project created for mutator

---
+++ 29 mutator-practice/app/Http/Controllers/MemberController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Member;
+
+class MemberController extends Controller
+{
+    function index(){
+        return Member::all();
+    }
+}
---
+++ 29 mutator-practice/app/Models/Member.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    use HasFactory;
+    public function getNameAttribute($value){
+        return ucfirst($value);
+    }
+
+
+    public function getAddressAttribute($value){
+        return $value.', India';
+    }
+}
