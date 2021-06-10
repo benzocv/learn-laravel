3f0f82944bba9034e97ff331490a5d2aae373d54
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 19:27:26 +0530

    data derived from the connected database done

--- 38 multiple-database-practice/routes/web.php
+++ 38 multiple-database-practice/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DeviceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('device',[DeviceController::class,'list']);
