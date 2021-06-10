375fe95e78f372a8c735f6d8d4d690203e0b3c57
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 23:15:18 +0530

    connection with model to another database done

--- 38 multiple-database-practice/app/Http/Controllers/DeviceController.php
+++ 38 multiple-database-practice/app/Http/Controllers/DeviceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Member;
 
 class DeviceController extends Controller
 {
@@ -11,7 +12,15 @@ class DeviceController extends Controller
     //  return DB::table('devices')->get();
 
      // to connect to extra connection database 
-     return DB::connection('mysql2')->table('employee')->get();
+    //  return DB::connection('mysql2')->table('employee')->get();
+
+
+
+
+
+
+    //method with model 
+    return Member::all();
     
  }
 }
