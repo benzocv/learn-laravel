ccb5b3b8db6fddc13d62cf86b216ca85f74e8680
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 18:28:24 +0530

    mail created

--- 36 markdown-mail-practice/routes/web.php
+++ 36 markdown-mail-practice/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Mail\SampleMail;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('mail', function () {
+    return new SampleMail();
 });
