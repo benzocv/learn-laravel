99ceb7618d9b37d423350b4f49e63a943a4d8f8b
benzocv <anzarahmedkhan99@gmail.com>
2021-06-10 17:52:33 +0530

    ond method added

--- 34 fluent-string-practice/routes/web.php
+++ 34 fluent-string-practice/routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Str;
 */
 
 $data = "hi, let's learn laravel";
-
+$data = Str::ucfirst($data);
+$data = Str::replaceFirst("Hi","Hello",$data);
+$data = Str::camel($data);
 
 echo $data;
 
