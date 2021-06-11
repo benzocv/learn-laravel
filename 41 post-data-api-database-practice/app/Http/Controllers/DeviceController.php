3285e803887c97616c304d45f1f8fc3eea3f9786
benzocv <anzarahmedkhan99@gmail.com>
2021-06-11 09:04:06 +0530

    controller working fine

---
+++ 41 post-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    function add(){
+        return ["Result"=>"Data has been saved"];
+    }
+}
