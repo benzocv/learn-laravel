d5129ee67960bc0f3b4939b323c03e5907adba08
benzocv <anzarahmedkhan99@gmail.com>
2021-06-11 09:14:11 +0530

    data successfully save done

--- 41 post-data-api-database-practice/app/Http/Controllers/DeviceController.php
+++ 41 post-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Device;
 
 class DeviceController extends Controller
 {
-    function add(){
-        return ["Result"=>"Data has been saved"];
+    function add(Request $req){
+        
+        $device = new Device;
+        $device->name = $req->name;
+        $device->member_id = $req->member_id;
+        $result = $device->save();
+        if($result){
+            
+            return ["Result"=>"Data has been saved"];
+
+        }else{
+            return ["Result"=>"Data save failed"];
+
+        }
     }
 }
