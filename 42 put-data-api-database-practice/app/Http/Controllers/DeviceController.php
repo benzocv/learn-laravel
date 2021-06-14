acb73f71a2cbde93c07a8ef286c775f3990a9387
benzocv <anzarahmedkhan99@gmail.com>
2021-06-14 09:54:44 +0530

    api setup and route connection done

---
+++ 42 put-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Device;
+
+class DeviceController extends Controller
+{
+    function update(){
+        return ["result"=>"data is updated"];
+    }
+}
