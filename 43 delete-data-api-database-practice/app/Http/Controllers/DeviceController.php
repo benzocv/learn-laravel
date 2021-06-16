df8649fd3781ac70f3c1d8ca1f02901bd3f6811b
benzocv <anzarahmedkhan99@gmail.com>
2021-06-16 23:51:22 +0530

    everyting works fine with update function

---
+++ 43 delete-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Device;
+
+class DeviceController extends Controller
+{
+    function delete(Request $req){
+        return ["result"=>"testing"];
+
+    }
+}
