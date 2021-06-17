e19f1d12b36450f53b10802ea9d11436d3b62b5c
benzocv <anzarahmedkhan99@gmail.com>
2021-06-17 23:34:15 +0530

    data delete done

--- 43 delete-data-api-database-practice/app/Http/Controllers/DeviceController.php
+++ 43 delete-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -8,7 +8,17 @@ use App\Models\Device;
 class DeviceController extends Controller
 {
     function delete($id){
-        return ["result"=>"testing".$id];
+        $device = Device::find($id);
+        $result = $device->delete();
+
+        if($result){
+            
+            return ["Result"=>"Data has been deleted"];
+
+        }else{
+            return ["Result"=>"Data delete failed"];
+
+        }
 
     }
 }
