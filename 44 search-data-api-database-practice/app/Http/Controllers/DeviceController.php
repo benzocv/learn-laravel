6c4a5c6ee6a2bbc8723391339885e7bebe67ce08
benzocv <anzarahmedkhan99@gmail.com>
2021-06-17 23:48:47 +0530

    model and controller added

---
+++ 44 search-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Device;
+
+class DeviceController extends Controller
+{
+    function search(){
+        return ['result'=>'result'];
+    }
+}
