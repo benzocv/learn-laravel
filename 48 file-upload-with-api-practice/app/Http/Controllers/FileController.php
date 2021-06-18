4eaeb6cb861b350a43f58ae2d4d81b4ee91fb304
benzocv <anzarahmedkhan99@gmail.com>
2021-06-18 15:48:57 +0530

    file upload not working

--- 48 file-upload-with-api-practice/app/Http/Controllers/FileController.php
+++ 48 file-upload-with-api-practice/app/Http/Controllers/FileController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    function upload(){
-        return ["result"=>"abcd"];
+    function upload(Request $req){
+        return $req->file('file')->store('docs');
+        // $result = $req->file('upload')->store('apiDocs');
+        // return ["result"=>$result];
     }
 }
