0f79091966a336554f6e00f5130a89784042be77
benzocv <anzarahmedkhan99@gmail.com>
2021-06-18 13:27:21 +0530

    database enty done

--- 47 sanctum-api-authentication-practice/database/seeders/UsersTableSeeder.php
+++ 47 sanctum-api-authentication-practice/database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,5 +16,11 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+
+        DB::table('users')->insert([
+            'name' => 'John Doe',
+            'email' => 'john@doe.com',
+            'password' => Hash::make('password')
+        ]);
     }
 }
