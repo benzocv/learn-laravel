6d7aa272d6413c648ed414a87e075cfe25edbe8b
benzocv <anzarahmedkhan99@gmail.com>
2021-06-18 12:09:33 +0530

    validation requried done

--- 45 validation-data-api-database-practice/app/Http/Controllers/DeviceController.php
+++ 45 validation-data-api-database-practice/app/Http/Controllers/DeviceController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Device;
+use Validator;
 
 class DeviceController extends Controller
 {
     function testData(Request $req){
-       return ['x'=>'y'];
+        $rules = array(
+            "member_id" => "required"
+        );
+        $validator = Validator::make($req->all(),$rules);
+        if($validator->fails()){
+            return $validator-> errors();
+        }else{
+            return ['x'=>$req];
+        }
+    //    return ['x'=>$req];
     }
 }
